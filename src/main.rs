f0ac3da90a2a4e389ed49a5baaf84f17d8d324a8
mateusns12 <mateusgms12@gmail.com>
2022-04-07 20:38:58 -0300

    Source code added
    main.rs created. .gitignore added. Target and build files are omitted.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let peripheral = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(peripheral);
+    let mut led = pins.d13.into_output().downgrade();
+
+    led.set_low();
+
+    loop {
+        led.toggle();
+        arduino_hal::delay_ms(100);
+    }
+}
